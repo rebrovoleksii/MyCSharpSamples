4d2f88b29777dbd7c33145479834d7fb6776eabf
arebrov <rebrovoleksii@gmail.com>
2014-06-20 18:57:21 +0300

    1. Added proj for playing with formats 2. Example on XML schema

---
+++ TypeConverstionAndDateFormats/TypeConverstionAndDateFormats/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TypeConverstionAndDateFormats
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd"));
+            Console.ReadLine();
+        }
+    }
+}
