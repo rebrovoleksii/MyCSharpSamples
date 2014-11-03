9d6bc975b6389c2302d49d1d246a207edb22d42a
arebrov <rebrovoleksii@gmail.com>
2014-11-03 19:19:53 +0200

    Added proj for ArtOfUnitTesting book

---
+++ TheArtOfUnitTesting/TheArtOfUnitTesting/LogAnalyzer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheArtOfUnitTesting
+{
+    class LogAnalyzer
+    {
+        public bool IsValidLogFileName(string fileName)
+        {
+            if (!fileName.ToLower().EndsWith(".slf"))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ TheArtOfUnitTesting/TheArtOfUnitTesting/LogAnalyzerTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace TheArtOfUnitTesting
+{
+    [TestFixture]
+    class LogAnalyzerTest
+    {
+        [Test]
+        public void IsValidFileName_validFile_ReturnsTrue()
+        {
+            var logAnalyzer = new LogAnalyzer();
+            var res = logAnalyzer.IsValidLogFileName("firsttest.SLF");
+            Assert.IsTrue(res,"file name should be valid");
+        }
+    }
+}
