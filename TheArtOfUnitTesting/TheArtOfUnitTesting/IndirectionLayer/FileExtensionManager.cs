92f11f6f7e0128e76577e1a7b902c26e9b2f3693
arebrov <rebrovoleksii@gmail.com>
2014-11-28 16:10:37 +0200

    Added files for IoC example

---
+++ TheArtOfUnitTesting/TheArtOfUnitTesting/IndirectionLayer/FileExtensionManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheArtOfUnitTesting
+{
+    class FileExtensionManager:IExtensionManager
+    {
+        public bool isValid(string fileName)
+        {
+            if (!fileName.ToLower().EndsWith(".slf"))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ TheArtOfUnitTesting/TheArtOfUnitTesting/IndirectionLayer/StubExtensionManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheArtOfUnitTesting
+{
+    class StubExtensionManager : IExtensionManager
+    {
+        public bool ShouldExtensionBeValid;
+
+        public bool isValid(string fileName)
+        {
+            return ShouldExtensionBeValid;
+        }
+    }
+}
