74f62abb44f8cde38c01c7696f3cb1eb1750297e
Alexey Rebrov <a.rebrov@NutsServices.local>
2016-06-24 17:15:52 +0200

    Added simple WPF window with 2 controls: TextBox and TextBlock

---
+++ ScreenTyper/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ScreenTyper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TextBlock userOutput;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBlock_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Initialize lable
+            userOutput = sender as TextBlock;
+        }
+
+        private void OnEnterPressedHandler(object sender, KeyEventArgs e)
+        {    
+            var textBox = sender as TextBox;    
+            if (e.Key == Key.Enter)
+            {
+                if (userOutput.Text != null)        
+                {
+                    var oldContent = userOutput.Text;
+                    userOutput.Text = oldContent + Environment.NewLine + textBox.Text;
+                    
+                }
+                else userOutput.Text = textBox.Text;
+                textBox.Clear();      
+            }                                                                          
+        }
+    }
+}
