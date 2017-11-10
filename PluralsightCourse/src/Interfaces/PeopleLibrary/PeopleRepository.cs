38aaae4349f03ae929ecb6cc8d54abe617731516
rebrovoleksii <rebrovoleksii@gmail.com>
2017-11-10 15:49:10 +0100

    Added example coding to interface

---
+++ PluralsightCourse/src/Interfaces/PeopleLibrary/PeopleRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PeopleLibrary
+{
+    public class PeopleRepository
+    {
+        public string [] GetPeopleList()
+        {
+            return new string[3] { "John Dow","Jane Dow", "Jogh Galt"};
+        }
+
+        public List<string> GetPeopleListNew()
+        {
+            return new List<string>() { "John Dow", "Jane Dow", "Jogh Galt" };
+        }
+    }
+}
---
+++ PluralsightCourse/src/Interfaces/PeopleViewer/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using PeopleLibrary;
+
+namespace PeopleViewer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private PeopleLibrary.PeopleRepository repository;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            repository = new PeopleRepository();
+        }
+
+        private void GetPeopleConcrete_OnClick(object sender, RoutedEventArgs e)
+        {
+            ClearPeopleList();
+
+            string[] peopleList;
+            peopleList = repository.GetPeopleList();
+            // this brakes
+            //peopleList = repository.GetPeopleListNew();
+            foreach (var person in peopleList)
+            {
+                this.PeopleList.Items.Add(person);
+            }
+        }
+
+        private void GetPeopleInterface_OnClick(object sender, RoutedEventArgs e)
+        {
+            ClearPeopleList();
+
+            //coding to interface
+            IEnumerable<string> peopleList;
+            peopleList = repository.GetPeopleList();
+            peopleList = repository.GetPeopleListNew();
+            foreach (var person in peopleList)
+            {
+                this.PeopleList.Items.Add("I " + person);
+            }
+        }
+
+        private void ClearList_OnClick(object sender, RoutedEventArgs e)
+        {
+            ClearPeopleList();
+        }
+
+        private void ClearPeopleList()
+        {
+            this.PeopleList.Items.Clear();
+        }
+    }
+}
